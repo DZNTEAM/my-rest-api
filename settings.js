8eb7b2172fff78a04609e193443594e7f61ea1fe
DZNTEAM <152410542+DZNTEAM@users.noreply.github.com>
2023-11-29 21:36:48 +0700

    settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'DZNTEAM'// yourname
+global.MONGO_DB_URI = "mongodb+srv://dznteampc:Dznteam31@cluster0.apat9ai.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "dznteamofficial" //email
+global.email_password = "gjfu klqe iadz kolx" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
